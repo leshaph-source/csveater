3fe08743f80a7c47d904e31fe21844eba2b85242
leshaph-source <leshaph@gmail.com>
2026-06-19 11:08:03 +1000

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,11 +1,11 @@
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 
-agent = create_pandas_dataframe_agent(
-    ChatOpenAI(
-        model="gpt-4o-mini",
-        temperature=0
-    ),
+llm = ChatOpenAI(
+    model="deepseek/deepseek-chat-v3-0324:free",
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY")
+),
     df,
     verbose=True,
     allow_dangerous_code=True
